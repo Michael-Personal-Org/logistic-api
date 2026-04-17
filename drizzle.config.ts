2c11518b76a44f39f52cc5a27b61cb11c94278ca
Michael Espinosa <alexanderbat89@gmail.com>
2026-04-17 15:30:34 -0400

    feat(users): deleting the commemted section on drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,3 @@
-// import { defineConfig } from 'drizzle-kit'
-// import { config } from 'dotenv'
-
-// config()
-
-// export default defineConfig({
-//   dialect: 'postgresql',
-//   schema: './src/features/users/infrastructure/db/user.schema.ts',
-//   out: './src/db/migrations',
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL!,
-//   },
-// })
-
 import { config } from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
 
